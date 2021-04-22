f81ee46eb943f815880afe3481fc519a32c60be6
Lozzio99 <lorenzopompigna1@gmail.com>
2021-04-22 08:59:37 +0200

    RungeKutta gravity sort of working
    Signed-off-by: Lozzio99 <lorenzopompigna1@gmail.com>

--- src/test/java/group17/phase1/Titan/Physics/ClockTest.java
+++ src/test/java/group17/phase1/Titan/Physics/ClockTest.java
@@ -1,10 +1,11 @@
 package group17.phase1.Titan.Physics;
 
 import group17.phase1.Titan.System.Clock;
-
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ClockTest {
 
@@ -12,9 +13,9 @@ class ClockTest {
     @DisplayName("StepSize1000")
     void StepSize1000() {
         Clock clock = new Clock();
-        clock.setCurrentDate(2020,4,1,0,0,0);
+        clock.setCurrentDate(2020, 4, 1, 0, 0, 0);
         clock.step(1000);
-        assertEquals(2020+"/"+4+"/"+1+"/"+" "+0+":"+16+":"+40, clock.toString());
+        assertEquals(2020 + "/" + 4 + "/" + 1 + "/" + " " + 0 + ":" + 16 + ":" + 40, clock.toString());
     }
 
     @Test
@@ -39,8 +40,35 @@ class ClockTest {
     @DisplayName("StepSizeYear")
     void StepSizeYear() {
         Clock clock = new Clock();
-        clock.setCurrentDate(2020,4,1,0,0,0);
-        clock.step(86400*365);
-        assertEquals(2021+"/"+4+"/"+1+"/"+" "+0+":"+0+":"+0, clock.toString());
+        clock.setCurrentDate(2020, 4, 1, 0, 0, 0);
+        clock.step(86400 * 365);
+        assertEquals(2021 + "/" + 4 + "/" + 1 + "/" + " " + 0 + ":" + 0 + ":" + 0, clock.toString());
+    }
+
+    @Test
+    @Disabled("FIX_ME")
+    @DisplayName("StepSizeDay")
+    void ContinuouslyUpdate() {
+        Clock clock = new Clock().getLaunchDay();
+        int[] daysInMonths = new int[]{30, 31, 30, 31, 31, 30, 31, 30, 31, 31, 28, 31};
+        double secInADay = 60 * 60 * 24; // 60sec * 60min * 24h
+        double[] allMonths = new double[daysInMonths.length];
+
+
+        int[] fromApril = new int[]{5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4};  //expected
+
+
+        for (int i = 0; i < daysInMonths.length; i++) {
+            allMonths[i] = secInADay * daysInMonths[i];  //step to take = (1day) * (days in this month)
+        }
+
+
+        for (int k = 0; k < 3; k++) //3 years
+            for (int i = 0; i < allMonths.length; i++)  //12 months
+            {
+                System.out.println("taking step : " + allMonths[i]);
+                clock.step(allMonths[i]);  //step 1 month
+                assertEquals(fromApril[i], clock.getMonth());
+            }
     }
 }
