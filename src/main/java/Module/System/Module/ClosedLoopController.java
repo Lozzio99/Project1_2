1e7c43fc1d563a65a1418d4863707801c97010c5
LourenceB <spy.x.eli.3@gmail.com>
2021-06-09 13:05:00 +0200

    Refactored controller structure

---
+++ src/main/java/Module/System/Module/ClosedLoopController.java
@@ -0,0 +1,11 @@
+package Module.System.Module;
+
+import Module.Math.Vector3dInterface;
+import Module.System.State.StateInterface;
+
+public class ClosedLoopController implements ControllerInterface{
+    @Override
+    public StateInterface<Vector3dInterface> controlFunction(StateInterface<Vector3dInterface> currentState, double currentTime) {
+        return null;
+    }
+}
